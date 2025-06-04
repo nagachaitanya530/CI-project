504cc4b99c5d48bfc80a704a954e233917dd9b34
nagachaitanya530 <nagachaitanya530@gmail.com>
2025-06-04 12:19:16 +0530

    added App.tsx and shadcnui components

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster as SonnerToaster } from "sonner";
+import Home from "./app/page";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+const queryClient = new QueryClient();
+
+const App = () => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <TooltipProvider>
+                <div className="min-h-screen bg-white text-black">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+
+                        </Routes>
+                    </BrowserRouter>
+                </div>
+            </TooltipProvider>
+        </QueryClientProvider>
+    )
+}
