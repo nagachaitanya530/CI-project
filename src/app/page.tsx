189c13c07dd206218378a0354fd2809a0fd56414
Harshadgore096 <131296785+Harshadgore096@users.noreply.github.com>
2025-06-10 11:34:17 +0530

    structure is changed for Routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,13 @@ import Navigation from "./_components/Navigation";
 import Footer from "./_components/Footer";
 import LanguageLandingPage from "./_components/Home";
 import Hero from "./_components/Hero";
-
 export default function Home() {
   return (
     <>
 
       <Navigation />
-      {/* <Hero /> */}
       <LanguageLandingPage />
-
+      <Hero />
       <Footer />
     </>
   );
