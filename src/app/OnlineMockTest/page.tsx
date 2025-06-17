e6f7ec6cac9365ddfe55c7b43a514c9bc37e2987
Govind <govindju@gmail.com>
2025-06-17 14:33:47 +0530

    online mock test page completed

--- src/app/OnlineMockTest/page.tsx
+++ src/app/OnlineMockTest/page.tsx
@@ -1,12 +1,12 @@
 import Navigation from "../_components/Navigation";
-
+import OnlineMockTest from "../_components/online-mock-test/OnlineMockTest";
 import Footer from "../_components/Footer";
 
-export default function JOC() {
+export default function OnlineMockTestPage() {
     return (
         <>
             <Navigation />
-            
+            <OnlineMockTest />
             <Footer />
         </>
     );
