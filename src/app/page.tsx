b7c2d39fab322faef0ae48e465337249628f9b3c
Ajmat <support@aalionsinfotech.com>
2025-06-17 17:37:15 +0530

    Lead Capture form added on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,20 @@ import Navigation from "./_components/Navigation";
 import Footer from "./_components/Footer";
 import LanguageLandingPage from "./_components/home/Home";
 import Hero from "./_components/Hero";
+import LeadCaptureModal from "./LeadCaptureModal";
 export default function Home() {
   return (
     <>
+       <div className="relative z-0 min-h-screen bg-white text-black">
+      
       <Navigation />
       <Hero />
       <LanguageLandingPage />
       <Footer />
+    
+     <LeadCaptureModal />
+     </div>
+    
     </>
   );
 }
