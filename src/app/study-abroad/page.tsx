374f69b440f2098d08b67b546f0a12835133f61f
nagachaitanya530 <nagachaitanya530@gmail.com>
2025-06-21 10:48:50 +0530

    navigation-bar created for study-abroad-section

---
+++ src/app/study-abroad/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from "~/app/_components/study-abroad/navigation";
+import OnlineMockTest from "../_components/online-mock-test/OnlineMockTest";
+
+export default function studyabroad() {
+    return (
+        <><Navigation />
+        </>
+    )
+
+}
