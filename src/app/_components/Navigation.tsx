03ad7cc3dd22ff8791f1a465aa009f8041b9abf7
Chandrashekhara <chandruny100@gmail.com>
2025-06-21 11:01:21 +0530

    Working on

--- src/app/_components/Navigation.tsx
+++ src/app/_components/Navigation.tsx
@@ -30,47 +30,25 @@ const navItems = [
 
       { label: "Special Classes", href: "/special-classes" },
       { label: "IELTS Classes", href: "/what-is-ielts" },
-
-      {
-        label: "English Courses",
-        dropdown: [
-
-          { label: "English Foundation Personal", href: "/" },
-          { label: "English Business Personal", href: "/" },
-          { label: "English Advanced Personal", href: "/english-advanced-course" },
-          { label: "Soft Skills Personal Training", href: "/" },
-          { label: "Corporate Training English", href: "/" },
+        
 
           {
             label: "English Courses",
             dropdown: [
 
               { label: "English Foundation Personal", href: "/english-foundation-course" },
-
-              { label: "English Foundation Personal", href: "/" },
-
               { label: "English Business Personal", href: "/english-business-course" },
-              { label: "English Advanced Personal", href: "/" },
+               { label: "English Advanced Personal", href: "/english-advanced-course" },
               { label: "Soft Skills Personal Training", href: "/" },
               { label: "Corporate Training English", href: "/" },
             ],
           },
 
-        ],
-      },
+    
 
       { label: "OET Classes", href: "/oet-classes" },
-      { label: "Foreign Languages", href: "/" },
-      { label: "Kids Courses (Languages)", href: "/" },
-
-      { label: "OET Classes", href: "/" },
       { label: "Foreign Languages", href: "/foreign-languages" },
-
       { label: "Kids Courses (Languages)", href: "/kids_courses" },
-
-      { label: "Online Mock Tests", href: "/" },
-
-      { label: "Kids Courses (Languages)", href: "/" },
       { label: "Online Mock Tests", href: "/OnlineMockTest" },
 
     ],
--- src/app/book-demo/main.tsx
+++ src/app/book-demo/main.tsx
@@ -95,8 +95,7 @@ const MainSection = () => {
   )
 }
 
-  );
-};
+
 
 
 export default MainSection;
