58b02e84d477ee376fafcde35466f2183cafeec0
Govind <govindju@gmail.com>
2025-06-21 17:20:39 +0530

    Your Journey component Completed

---
+++ src/app/study-abroad/India/page.tsx
@@ -0,0 +1,13 @@
+import Navigation from "~/app/_components/study-abroad/navigation";
+import YourJourney from "~/app/_components/study-abroad/your-journey";
+
+export default function India() {
+    return (
+        <>
+        <Navigation />
+        <YourJourney />
+
+        </>
+    )
+
+}
