8d4fb3032b48a52c08fa903a2619e983fd84a225
Ankitpanchal123 <ankit.panchal.t9698@aalionsinfotech.com>
2025-07-03 13:25:40 +0530

    Optimization of all code completed

--- src/app/_components/partnered-universities/CountryGrid.tsx
+++ src/app/_components/partnered-universities/CountryGrid.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState ,useEffect } from "react";
+import { useState, useEffect } from "react";
 import SideBarSection from "./SideBar";
 import UniversityList from "./UniversityCarousel";
 
@@ -36,124 +36,97 @@ const countries: CountryKey[] = [
 
 const universitiesByCountry: Record<CountryKey, { image: string; name: string }[]> = {
   "USA": [
-    { image: "/", name: "Massachusetts Institute of Technology (MIT)" },
-    { image: "/", name: "Stanford University" },
-    { image: "/", name: "Harvard University" },
-    { image: "/", name: "California Institute of Technology (Caltech)" },
-    { image: "/", name: "University of Chicago" },
-    { image: "/", name: "University of Pennsylvania" },
-    { image: "/", name: "Columbia University" },
-    { image: "/", name: "Yale University" },
-    { image: "/", name: "University of California, Berkeley" },
-    { image: "/", name: "Princeton University" }
+    { image: "/university/Massachusetts_Institute_of_Technology-Logo.png", name: "Massachusetts Institute of Technology (MIT)" },
+    { image: "/study-abroad/su.jpg", name: "Stanford University" },
+    
   ],
   "UK": [
-    { image: "/", name: "Birkbeck University of London" },
-    { image: "/", name: "Birmingham City University International College" },
-    { image: "/", name: "Bristol University" },
-    { image: "/", name: "Brunel University" },
-    { image: "/", name: "Buckinghamshire New University" },
-    { image: "/", name: "Canterbury Christ Church University" },
-    { image: "/", name: "Cardiff Metropolitan University" },
-    { image: "/", name: "Cardiff University" },
-    { image: "/", name: "City University of London" },
-    { image: "/", name: "Coventry University" },
-    { image: "/", name: "University of Oxford" },
-    { image: "/", name: "University of Cambridge" },
-    { image: "/", name: "Imperial College London" },
-    { image: "/", name: "University College London (UCL)" },
-    { image: "/", name: "London School of Economics and Political Science (LSE)" }
+    { image: "/university/Birkbeck-University-of-London.png", name: "Birkbeck University of London" },
+    { image: "/university/bcu-birmingham-city-university.jpg", name: "Birmingham City University International College" },
+   
   ],
   "Canada": [
-    { image: "/", name: "University of Toronto" },
-    { image: "/", name: "University of British Columbia" },
-    { image: "/", name: "McGill University" },
-    { image: "/", name: "University of Alberta" },
-    { image: "/", name: "University of Montreal" }
+    { image: "/university/University_of_Toronto-Logo.wine.png", name: "University of Toronto" },
+    { image: "/university/university-of-british-columbia.png", name: "University of British Columbia" },
+  
   ],
   "Australia": [
-    { image: "/", name: "University of Melbourne" },
-    { image: "/", name: "Australian National University (ANU)" },
-    { image: "/", name: "University of Sydney" },
-    { image: "/", name: "University of Queensland" },
-    { image: "/", name: "University of New South Wales (UNSW Sydney)" }
+    { image: "/university/University of Melbourne.png", name: "University of Melbourne" },
+    { image: "/university/Australian National University (ANU).png", name: "Australian National University (ANU)" },
+ 
   ],
   "Germany": [
-    { image: "/", name: "Technical University of Munich" },
-    { image: "/", name: "Ludwig Maximilian University of Munich" },
-    { image: "/", name: "Heidelberg University" },
-    { image: "/", name: "Humboldt University of Berlin" },
-    { image: "/", name: "RWTH Aachen University" }
+    { image: "/university/Technical University of Munich.png", name: "Technical University of Munich" },
+    { image: "/university/Ludwig Maximilian University of Munich.png", name: "Ludwig Maximilian University of Munich" },
+
   ],
   "Switzerland": [
-    { image: "/", name: "ETH Zurich - Swiss Federal Institute of Technology" },
-    { image: "/", name: "École Polytechnique Fédérale de Lausanne (EPFL)" }
+    { image: "/university", name: "ETH Zurich - Swiss Federal Institute of Technology" },
+    { image: "/university", name: "École Polytechnique Fédérale de Lausanne (EPFL)" }
   ],
   "Singapore": [
-    { image: "/", name: "National University of Singapore (NUS)" },
-    { image: "/", name: "Nanyang Technological University (NTU)" }
+    { image: "/university/National University of Singapore (NUS).png", name: "National University of Singapore (NUS)" },
+    { image: "/university/Nanyang Technological University (NTU).png", name: "Nanyang Technological University (NTU)" }
   ],
   "Netherlands": [
-    { image: "/", name: "Delft University of Technology" },
-    { image: "/", name: "University of Amsterdam" },
-    { image: "/", name: "Eindhoven University of Technology" }
+    { image: "/university/Delft University of Technology.png", name: "Delft University of Technology" },
+
   ],
   "France": [
-    { image: "/", name: "Université PSL (Paris Sciences & Lettres)" },
-    { image: "/", name: "École Polytechnique" },
-    { image: "/", name: "Sorbonne University" }
+    { image: "/university/Université PSL (Paris Sciences & Lettres).png", name: "Université PSL (Paris Sciences & Lettres)" },
+    { image: "/university/École Polytechnique.png", name: "École Polytechnique" },
+    { image: "/university/Sorbonne University.png", name: "Sorbonne University" }
   ],
   "India": [
-    { image: "/", name: "Indian Institute of Technology Bombay (IITB)" },
-    { image: "/", name: "Indian Institute of Science (IISc) Bangalore" },
-    { image: "/", name: "Indian Institute of Technology Delhi (IITD)" }
+    { image: "/university/Indian Institute of Technology Bombay (IITB).png", name: "Indian Institute of Technology Bombay (IITB)" },
+    { image: "/university/Indian Institute of Science (IISc) Bangalore.png", name: "Indian Institute of Science (IISc) Bangalore" },
   ],
   "Ireland": [
-    { image: "/", name: "Trinity College Dublin" },
-    { image: "/", name: "University College Dublin" }
+    { image: "/university/Trinity College Dublin.jpeg", name: "Trinity College Dublin" },
+    { image: "/university/University College Dublin.png", name: "University College Dublin" }
   ],
   "New Zealand": [
-    { image: "/", name: "University of Auckland" },
-    { image: "/", name: "University of Otago" }
+    { image: "/university/university-of-auckland.png", name: "University of Auckland" },
+    { image: "/university/University of Otago Doctoral.jpeg", name: "University of Otago" }
   ],
   "Spain": [
-    { image: "/", name: "University of Barcelona" },
-    { image: "/", name: "Autonomous University of Madrid" }
+    { image: "/university/university-of-barcelona.jpeg", name: "University of Barcelona" },
+    { image: "/university/Autonomous University of Madrid.png", name: "Autonomous University of Madrid" }
   ],
   "Italy": [
-    { image: "/", name: "University of Bologna" },
-    { image: "/", name: "Sapienza University of Rome" }
+    { image: "/university/University-of-Bologna.jpeg", name: "University of Bologna" },
+    { image: "/university/Sapienza University of Rome.jpeg", name: "Sapienza University of Rome" }
   ],
   "Malaysia": [
-    { image: "/", name: "University of Malaya (UM)" },
-    { image: "/", name: "Universiti Putra Malaysia (UPM)" }
+    { image: "/university/university-of-malaya.png", name: "University of Malaya (UM)" },
+    { image: "/university/university-of-malaysia.png", name: "Universiti Putra Malaysia (UPM)" }
   ],
   "Poland": [
-    { image: "/", name: "University of Warsaw" },
-    { image: "/", name: "Jagiellonian University" }
+    { image: "/university/University of Warsaw Logo.jpeg", name: "University of Warsaw" },
+    { image: "/university/Jagiellonian University logo.png", name: "Jagiellonian University" }
   ],
   "Finland": [
-    { image: "/", name: "University of Helsinki" },
-    { image: "/", name: "Aalto University" }
+    { image: "/university/university-of-helsinki.jpeg", name: "University of Helsinki" },
+    { image: "/university/Aalto University.jpeg", name: "Aalto University" }
   ],
   "Hungary": [
-    { image: "/", name: "Eötvös Loránd University" },
-    { image: "/", name: "University of Szeged" }
+    { image: "/university/eotvos-lorand-university.png", name: "Eötvös Loránd University" },
+    { image: "/university/UOS.avif", name: "University of Szeged" }
   ],
   "Dubai": [
-    { image: "/", name: "University of Dubai" },
-    { image: "/", name: "American University in Dubai" }
+    { image: "/university/dubai.png", name: "University of Dubai" },
+    { image: "/university/aud.jpg", name: "American University in Dubai" }
   ],
   "Mauritius": [
-    { image: "/", name: "University of Mauritius" },
-    { image: "/", name: "Middlesex University Mauritius" }
+    { image: "/university/UOM.jpg", name: "University of Mauritius" },
+    { image: "/university/MUM.jpg", name: "Middlesex University Mauritius" }
   ],
   "Malta": [
-    { image: "/", name: "University of Malta" }
+    { image: "/university/UOMA.png", name: "University of Malta" }
   ],
   "International": [
-    { image: "/", name: "Jacobs University Bremen" },
-    { image: "/", name: "Schiller International University" }
+    { image: "/university/JUB.png", name: "Jacobs University Bremen" },
+    { image: "/university/SIU.png", name: "Schiller International University" }
   ]
 };
 
@@ -188,17 +161,14 @@ function CountryUniversity() {
 
   return (
     <section className="mx-4 sm:mx-6 md:mx-10 lg:mx-25">
-
-      {/* Responsive Navigation - shows only on sm and md */}
       <div className="lg:hidden overflow-x-auto whitespace-nowrap my-4 py-2 px-2 border rounded-md shadow-sm scroll-smooth">
         <ul className="flex gap-3">
           {countries.map((country) => (
             <li
               key={country}
               onClick={() => handleSelect(country)}
-              className={`cursor-pointer px-4 py-2 rounded-md whitespace-nowrap ${
-                selectCountry === country ? 'bg-cyan-700 text-white' : 'bg-gray-100 hover:bg-cyan-100'
-              }`}
+              className={`cursor-pointer px-4 py-2 rounded-md whitespace-nowrap ${selectCountry === country ? 'bg-cyan-700 text-white' : 'bg-gray-100 hover:bg-cyan-100'
+                }`}
             >
               {country}
             </li>
@@ -207,7 +177,6 @@ function CountryUniversity() {
       </div>
 
       <div className="flex gap-5">
-        {/* Sidebar - only visible on lg and up */}
         <div className="hidden lg:block w-[17%]">
           <SideBarSection
             countries={countries}
@@ -215,10 +184,7 @@ function CountryUniversity() {
             selectedCountry={selectCountry}
           />
         </div>
-
-        {/* Main Content */}
         <div className="w-full">
-          {/* Search */}
           <div className="flex justify-end  gap-3 my-5">
             <input
               type="text"
@@ -231,36 +197,30 @@ function CountryUniversity() {
               Search
             </button>
           </div>
-
-          {/* Scrollable University List */}
           <div className="my-4">
             <UniversityList universiteslist={currentUniversities} />
           </div>
-
         </div>
       </div>
-          {/* Pagination */}
-          {/* {filteredUniversities.length > itemsPerPage && ( */}
-            <div className="flex justify-center mt-4 gap-3">
-              <button
-                onClick={handlePrev}
-                disabled={currentPage === 1}
-                className="bg-gray-200 px-4 py-2 rounded disabled:opacity-50"
-              >
-                Prev
-              </button>
-              <span className="px-2 py-1">
-                Page {currentPage} of {totalPages}
-              </span>
-              <button
-                onClick={handleNext}
-                disabled={currentPage === totalPages}
-                className="bg-gray-200 px-4 py-2 rounded disabled:opacity-50"
-              >
-                Next
-              </button>
-            </div>
-       
+      <div className="flex justify-center mt-4 gap-3">
+        <button
+          onClick={handlePrev}
+          disabled={currentPage === 1}
+          className="bg-gray-200 px-4 py-2 rounded disabled:opacity-50"
+        >
+          Prev
+        </button>
+        <span className="px-2 py-1">
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={handleNext}
+          disabled={currentPage === totalPages}
+          className="bg-gray-200 px-4 py-2 rounded disabled:opacity-50"
+        >
+          Next
+        </button>
+      </div>
     </section>
   );
 }
