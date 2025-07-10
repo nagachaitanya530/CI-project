1cfebe7bcffaea635316654384879707081b0536
nagachaitanya530 <nagachaitanya530@gmail.com>
2025-07-10 09:06:17 +0530

    resloved all the conflicts from ci and studyabroad and everypage is working

---
+++ src/app/What-we-do/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useState } from "react";
+import NavigationSection from "../_components/navigation1";
+import FooterSection from "../_components/footer1";
+import Whathero from "../What-we-do/whathero"
+import YourJourney from "../_components/study-abroad/your-journey";
+import Afterhero from "../_components/afterhero";
+import ConsultationFormModal from '~/app/_components/study-abroad/study-in-spain/ConsultationForm';
+export default function whatwedo() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+
+    <>
+      <ConsultationFormModal show={showModal} onClose={() => setShowModal(false)} />
+      <NavigationSection />
+      <Whathero onFreeConsultClick={() => setShowModal(true)} />
+      <Afterhero />
+      <YourJourney />
+      <FooterSection />
+    </>
+  )
+};
+
